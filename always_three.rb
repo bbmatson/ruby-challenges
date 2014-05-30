d435ad542f58660b9872340ed141ac7a0fa134b3
Barbara Matson <barbaramatson@usa.net>
2014-05-30 00:15:34 -0400

    Commits my first two files

---
+++ always_three.rb
@@ -0,0 +1,9 @@
+puts "Give me a number"
+number = gets.to_i
+def always_three(number)
+
+puts  "It's always #{((((number + 5) *2) - 4) / 2) - number}"
+end
+always_three(number)
+
+
---
+++ weather.rb
@@ -0,0 +1,14 @@
+puts "what's the weather?"
+
+weather = 'snowy'
+
+case weather
+when 'sunny'
+    puts "Lift your face to the sky"
+when 'rainy'
+    puts "Bring an umbrella"
+when 'windy'
+    puts "Batten down the hatches"
+when 'snowy'
+    puts "Wear a toque"
+    end
