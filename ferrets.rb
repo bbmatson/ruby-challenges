cf727528c366007a159cf35bd80fa779e7bd6f91
Barbara Matson <barbaramatson@usa.net>
2014-06-01 22:48:21 -0400

    my new commit

---
+++ ferrets.rb
@@ -0,0 +1,68 @@
+class Pet
+ 
+    def set_name=(ferret_name)
+        @name = ferret_name
+    end
+ 
+    def get_name
+        return @name
+    end
+ 
+    def set_owner=(owner_name)
+        @owner_name = owner_name
+    end
+ 
+    def get_owner
+        return @owner_name
+    end
+    end
+ class Ferret < Pet
+    def squeal
+        return "squeeeeee"
+    end
+ 
+end
+ 
+class Chincilla < Pet
+ 
+    
+ 
+    def squeek
+        return "eeeep"
+    end
+ end
+ 
+class Parrot< Pet
+ 
+    
+    def tweet
+        return "tweet"
+        end
+end
+ 
+my_ferret = Ferret.new
+my_ferret.set_name= "Fredo"
+ferretname = my_ferret.get_name
+my_ferret.set_owner = "Judy"
+ferretowner = my_ferret.get_owner
+
+
+my_parrot = Parrot.new
+my_parrot.set_name="Budgie"
+parrotname = my_parrot.get_name
+my_parrot.set_owner = "Hortense"
+parrotowner = my_parrot.get_owner
+
+ 
+my_chincilla = Chincilla.new
+my_chincilla.set_name= "Dali"
+chincillaname = my_chincilla.get_name
+ 
+puts "#{ferretname}, who is owned by #{ferretowner}, says #{my_ferret.squeal}, 
+#{parrotname}, who is owned by #{parrotowner}, says #{my_parrot.tweet}, 
+and #{chincillaname} says #{my_chincilla.squeek}."
+ 
+puts my_ferret.inspect
+puts my_parrot.inspect
+puts my_chincilla.inspect
+
